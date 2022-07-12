4d177506de34c288b9c9acab0311f98253f64c2e
AnastasiaPsarou <97515093+AnastasiaPsarou@users.noreply.github.com>
2022-07-12 17:42:41 +0200

    added_*four_image

--- ce326/hw2/ImageProcessing2.java
+++ ce326/hw2/ImageProcessing2.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class ImageProcessing2 {
 
-  public static void main(String []args) {
+  public static void main(String[] args) {
     PPMImage rgbImg = null;
     Scanner sc = new Scanner(System.in);
     YUVImage yuvImg = null;
@@ -14,36 +14,35 @@ public class ImageProcessing2 {
     do {
       System.out.print(menu());
       String input = sc.next();
-      switch(input.substring(0,1).toLowerCase()) {
+      switch (input.substring(0, 1).toLowerCase()) {
         case "i":
           System.out.print("Enter filename to import: ");
           filename = sc.next();
-          try(Scanner fsc = new Scanner(new File(filename))) {
+          try (Scanner fsc = new Scanner(new File(filename))) {
             String magicNumber = fsc.next();
-            if(magicNumber.equals("P3"))
+            if (magicNumber.equals("P3"))
               rgbImg = new PPMImage(new File(filename));
-            else if(magicNumber.equals("YUV3")) {
+            else if (magicNumber.equals("YUV3")) {
               yuvImg = new YUVImage(new File(filename));
               rgbImg = new PPMImage(yuvImg);
-            }
-            else
+            } else
               System.out.println("\nERROR: Invalid magic number for input filename!\n");
-          } catch(FileNotFoundException ex) {
-            System.err.println("File \""+filename+"\" was not found!");
-          } catch(UnsupportedFileFormatException ex) {
-            System.out.format("Incorrectly formatted file '%s'%n"+filename);
+          } catch (FileNotFoundException ex) {
+            System.err.println("File \"" + filename + "\" was not found!");
+          } catch (UnsupportedFileFormatException ex) {
+            System.out.format("Incorrectly formatted file '%s'%n" + filename);
           }
           break;
         case "e":
           System.out.println("Export as:\n\t(p)PPM file\n\t(y)YUV file\n");
           input = sc.next();
-          switch(input.toLowerCase().substring(0,1)) {
+          switch (input.toLowerCase().substring(0, 1)) {
             case "p":
               System.out.print("Enter PPM filename to export to: ");
               filename = sc.next();
-              file = new File(filename);           
+              file = new File(filename);
               rgbImg.toFile(file);
-              break;            
+              break;
             case "y":
               System.out.print("Enter YUV filename to export to: ");
               filename = sc.next();
@@ -51,7 +50,7 @@ public class ImageProcessing2 {
               yuvImg = new YUVImage(rgbImg);
               yuvImg.toFile(file);
               break;
-          }          
+          }
           break;
         case "r":
           rgbImg.rotateClockwise();
@@ -59,22 +58,25 @@ public class ImageProcessing2 {
         case "d":
           rgbImg.doublesize();
           break;
+        case "f":
+          rgbImg.foursize();
+          break;
         case "h":
           rgbImg.halfsize();
           break;
         case "g":
           rgbImg.grayscale();
-          break;        
+          break;
         case "z":
-          yuvImg = new YUVImage(rgbImg);          
-          yuvImg.equalize();          
-          rgbImg = new PPMImage(yuvImg);          
+          yuvImg = new YUVImage(rgbImg);
+          yuvImg.equalize();
+          rgbImg = new PPMImage(yuvImg);
           break;
         case "p":
-          yuvImg = new YUVImage(rgbImg);          
+          yuvImg = new YUVImage(rgbImg);
           Histogram h = new Histogram(yuvImg);
           System.out.println(h.toString());
-          rgbImg = new PPMImage(yuvImg);          
+          rgbImg = new PPMImage(yuvImg);
           break;
         case "s":
           System.out.println("Enter directory to load images from: ");
@@ -84,21 +86,19 @@ public class ImageProcessing2 {
             PPMImageStacker stacker = new PPMImageStacker(directory);
             stacker.stack();
             rgbImg = stacker.getStackedImage();
-          }
-          catch(FileNotFoundException ex) {
+          } catch (FileNotFoundException ex) {
             System.out.format("Unable to locate directory '%s'%n", dirname);
-          }
-          catch(UnsupportedFileFormatException ex) {
-            System.out.format("Incorrectly formatted file inside directory '%s'%n"+dirname);
+          } catch (UnsupportedFileFormatException ex) {
+            System.out.format("Incorrectly formatted file inside directory '%s'%n" + dirname);
           }
           break;
         case "q":
           return;
         default:
       }
-    } while(true);
+    } while (true);
   }
-  
+
   public static String menu() {
     StringBuffer str = new StringBuffer();
     str.append("+------  MENU ------+\n");
@@ -106,6 +106,7 @@ public class ImageProcessing2 {
     str.append("(e)xport image\n");
     str.append("(r)otate image\n");
     str.append("(d)ouble image size\n");
+    str.append("(f)our image size\n");
     str.append("(h)alf image size\n");
     str.append("(g)rayscale image\n");
     str.append("(z)equalize histogram\n");
